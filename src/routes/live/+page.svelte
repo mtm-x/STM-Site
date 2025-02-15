db9ed5b31d8e0ca774af7cc640228c16abbebc57
GJaswin <gjaswink@gmail.com>
2025-02-16 00:04:55 +0530

    Init

---
+++ src/routes/live/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<main class="relative top-[6rem] p-3">
+  <div class="outer-container">
+    <div class="grid grid-cols-2 gap-5">
+      <div>
+        <h2 class="text-xl font-bold text-gray-600 mb-3">Traffic Junction 1</h2>
+        <div class="feed-canvas bg-gray-200 w-[100%] h-100 rounded"></div>
+      </div>
+      <div>
+        <h2 class="text-xl font-bold text-gray-600 mb-3">Traffic Junction 2</h2>
+        <div class="feed-canvas bg-gray-200 w-[100%] h-100 rounded"></div>
+      </div>
+      <div>
+        <h2 class="text-xl font-bold text-gray-600 mb-3">Traffic Junction 3</h2>
+        <div class="feed-canvas bg-gray-200 w-[100%] h-100 rounded"></div>
+      </div>
+      <div>
+        <h2 class="text-xl font-bold text-gray-600 mb-3">Traffic Junction 4</h2>
+        <div class="feed-canvas bg-gray-200 w-[100%] h-100 rounded"></div>
+      </div>
+    </div>
+  </div>
+</main>
