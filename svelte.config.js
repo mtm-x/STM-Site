7ea9d0e08e5b69fca1ef7c814bdd189ad54e35c7
GJaswin <gjaswink@gmail.com>
2025-02-18 17:59:10 +0000

    Added multi adapter support

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
-import adapter from 'svelte-adapter-bun';
+import 'dotenv/config'
+import adapterBun from 'svelte-adapter-bun';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNetlify from '@sveltejs/adapter-netlify'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +15,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: (() => {
+			switch (process.env.ENVIRONMENT) {
+				case 'bun':
+					return adapterBun();
+				case 'node':
+					return adapterNode();
+				case 'netlify':
+					return adapterNetlify();
+				default:
+					return adapterAuto();
+			}
+		})()
 	}
 };
 
