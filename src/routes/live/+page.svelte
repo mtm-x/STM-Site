1de17e1c14ba151c38979c2f8ba5a8732e1c0cb7
mtm-x <mahasel.1969@gmail.com>
2025-03-01 22:33:13 +0530

    added cloudflare random tunnel

--- src/routes/live/+page.svelte
+++ src/routes/live/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  let imageUrl = '';
+
+  async function fetchLatestImage() {
+    imageUrl = `https://return-attribute-march-mixture.trycloudflare.com/latest-image?timestamp=${new Date().getTime()}`;
+  }
+
+  onMount(() => {
+    fetchLatestImage();
+    const interval = setInterval(fetchLatestImage, 5000); // Refresh every 5 seconds
+    return () => clearInterval(interval);
+  });
 </script>
 
-<main class="relative top-[5rem] p-3">
+<main class="relative top-[5rem]">
   <div class="outer-container">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div>
         <h2 class="text-xl font-bold text-gray-600 mb-3">Traffic Junction 1</h2>
-        <div class="feed-canvas bg-gray-200 w-[100%] h-100 rounded"></div>
+        <div class="feed-canvas bg-gray-200 w-[100%] h-100 rounded">
+          <img src={imageUrl} alt="Latest Detection" on:error={() => imageUrl = ''} style="max-width: 100%; height: auto;" />
+        </div>
       </div>
       <div>
         <h2 class="text-xl font-bold text-gray-600 mb-3">Traffic Junction 2</h2>
